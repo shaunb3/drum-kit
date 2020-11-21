24d0fa16a7fe151940b2d43f874433bc3dd1a659
Shaun <shaunbw4@gmail.com>
2020-11-21 21:23:53 +0200

    Updates

---
+++ precache-manifest.bc0a6e23681257ea2a9c2c5e2dc6a897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7a6fca00be896dbc00b107303f34ca",
+    "url": "/shaunb3/drum-kit/index.html"
+  },
+  {
+    "revision": "1fef60b91d2c8d69db8a",
+    "url": "/shaunb3/drum-kit/static/css/2.0b8fbf56.chunk.css"
+  },
+  {
+    "revision": "fc6ad18db35cc81b8d48",
+    "url": "/shaunb3/drum-kit/static/css/main.c44a443e.chunk.css"
+  },
+  {
+    "revision": "1fef60b91d2c8d69db8a",
+    "url": "/shaunb3/drum-kit/static/js/2.a17e7326.chunk.js"
+  },
+  {
+    "revision": "fc6ad18db35cc81b8d48",
+    "url": "/shaunb3/drum-kit/static/js/main.f3cad5cc.chunk.js"
+  },
+  {
+    "revision": "0fb6d62deddb54455b45",
+    "url": "/shaunb3/drum-kit/static/js/runtime-main.90debf44.js"
+  }
+]);
