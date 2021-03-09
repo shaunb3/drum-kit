eccaa7945f9385f202ed1ba4f868dcd90f5e2847
ris3n <risensx22@protonmail.com>
2021-03-09 22:39:20 +0000

    removed tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Drumpad from './components/Drumpad.js';
-import ReactFCCtest from 'react-fcctest';
+
 
 import './App.css';
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Drumpad />
-      <ReactFCCtest />
+      
     </div>
   );
 }
